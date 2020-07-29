72b8bd6c29ebc7f09ee583331705f0e2f55eab1c
EJon-bit <59263624+EJon-bit@users.noreply.github.com>
2020-07-29 17:47:02 -0500

    test files added

---
+++ pirtest.py
@@ -0,0 +1,41 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BCM)
+
+PIR_PIN= 18
+
+GPIO.setup(PIR_PIN, GPIO.IN)
+
+pirOne=0
+
+def MOTION(PIR_PIN):
+    if GPIO.input(PIR_PIN):     # if pin input high  
+        pirOne=1
+    else:                  # if pin input low 
+        pirOne=2
+
+
+try:
+    GPIO.add_event_detect(PIR_PIN, GPIO.BOTH, callback=MOTION)
+
+    while 1:
+
+        if pirOne==1:
+            print('Motion Detected')
+
+        else:
+            print('No motion Detected')
+
+except KeyboardInterrupt:  
+    # here you put any code you want to run before the program   
+    # exits when you press CTRL+C  
+    print('Exit Code')
+
+
+except Exception as e:
+    print(e)
+
+finally:
+        
+    GPIO.cleanup()
