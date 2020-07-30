7cd528efab531787efa156430b226fa0c98ef9b3
EJon-bit <59263624+EJon-bit@users.noreply.github.com>
2020-07-29 19:51:37 -0500

    cant2

--- pirtest.py
+++ pirtest.py
@@ -11,7 +11,7 @@ GPIO.setup(PIR_PIN, GPIO.IN)
 pirOne=0
 
 def MOTION(PIR_PIN):
-    global pir_one
+    global pirOne
     pirOne=1
     print('Pir One is',pirOne)
     
@@ -22,7 +22,7 @@ print ('Ready')
 
 try:
     GPIO.add_event_detect(PIR_PIN, GPIO.RISING, callback=MOTION)
-    while 1:
+    while 1:        
         if pirOne==1:
             print ('Motion Detected!')
         else:
