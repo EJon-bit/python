f43a22f543569b81de855436c9ca65fc1fd2d3dd
EJon-bit <59263624+EJon-bit@users.noreply.github.com>
2020-07-30 20:03:24 -0500

    update

---
+++ test.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+
+BUZZ_1PIN=7
+GPIO.setup(BUZZ_1PIN, GPIO.OUT)
+
+try:  
+    
+    while 1: 
+        i=0
+
+        while i<=3:
+            print(i)
+            print('Buzzing')
+            GPIO.output(BUZZ_1PIN, GPIO.HIGH)
+            time.sleep(2);
+            GPIO.output(BUZZ_1PIN, GPIO.LOW)
+            time,sleep(2)
+            i+=1
+            
+
+except KeyboardInterrupt:  
+   print('Exit')
+
+
+except Exception as e:
+    print(e)
+
+finally:
+
+    GPIO.cleanup()
